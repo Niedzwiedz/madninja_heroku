0a2eb8559ef243e7b669f87da9bfff9a944850c5
Michael Nov <m93.autumn@gmail.com>
2016-06-03 16:49:12 +0200

    reCaptcha env in heroku

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6LczmyETAAAAAOMD9IpCBX4MXdA9XAC4KrR8bO8y'
-  config.private_key = '6LczmyETAAAAAE3l28UQ8RHnK2Fr3L6JpmoEO36X'
+  config.public_key  = ENV['RECAPTCHA_PUBLIC_KEY']
+  config.private_key = ENV['RECAPTCHA_PRIVATE_KEY']
 end
